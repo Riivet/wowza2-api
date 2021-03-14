b844d76cdf6ba84a21df089e2036158edcfc5a14
Ramiro Rodriguez <ramiro.rodriguez@gmail.com>
2021-03-14 00:08:48 -0500

    add ability to retrieve a stream target without knowing the type

--- lib/wowza/api/stream_target/base.rb
+++ lib/wowza/api/stream_target/base.rb
@@ -1,5 +1,22 @@
 module Wowza::Api
   module StreamTarget
+    def self.retrieve(id)
+      types = [
+        Wowza::Api::StreamTarget::Fastly,
+        Wowza::Api::StreamTarget::Akamai,
+        Wowza::Api::StreamTarget::Custom,
+        Wowza::Api::StreamTarget::Facebook
+      ]
+      types.each do |type|
+        begin
+          stream_target = type.retrieve id
+          return stream_target
+        rescue
+        end
+      end
+      return nil
+    end
+
     class Base < Wowza::Api::Base
       def self.list
         response = get('/stream_targets')
--- lib/wowza/api/version.rb
+++ lib/wowza/api/version.rb
@@ -1,5 +1,5 @@
 module Wowza
   module Api
-    VERSION = "0.1.5"
+    VERSION = "0.1.6"
   end
 end
